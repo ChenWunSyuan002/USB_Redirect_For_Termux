5eac609af24b7459f18df5df2607d9bf8e022fc7
Michael Hanselmann <public@hansmi.ch>
2021-03-31 23:39:49 +0200

    Make fuzzing with meson build work
    TL;DR: Update build system and utility script such invoking
    `build-aux/oss-fuzz.sh` is all that's needed to build fuzzers in an OSS-Fuzz
    compatible environment.
    
    `.gitlab-ci.yml`: Build three variants of fuzzers: using autoconf as before,
    libFuzzer via Clang and using the standalone driver.
    
    `build-aux/oss-fuzz.sh`: Remove compiler-specific code. The fuzzing environment
    should control the compiler via `$CC`/`$CXX` along with flags in
    `$CFLAGS`/`$CXXFLAGS`. Make code actually build by overriding `b_lundef` default
    value, otherwise fuzzing-related symbols would cause linker errors during
    compilation. Pass fuzzing engine via option; the default would be to use the
    built-in standalone engine.
    
    `meson.build`: Remove all fuzzing-related logic except to detect whether fuzzing
    should be enabled at all.
    
    `fuzzing/meson.build`: If fuzzing engine `standalone` is selected the build
    behaves as any other C program build. When another fuzzing engine is to be used,
    e.g. from `$LIB_FUZZING_ENGINE` in `build-aux/oss-fuzz.sh`, a test program is
    compiled and linked to ensure that linking suceeds (in particular whether
    a `main` function is provided). Prepare for the addition of more fuzzing
    binaries by using a loop. Add OSS-Fuzz configuration file for binaries.
    
    Signed-off-by: Michael Hanselmann <public@hansmi.ch>

--- build-aux/oss-fuzz.sh
+++ build-aux/oss-fuzz.sh
@@ -1,41 +1,39 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: LGPL-2.1-or-later
-# Original from: https://github.com/systemd/systemd/blob/main/tools/oss-fuzz.sh
 
-
-set -ex
+set -eux -o pipefail
 
 export LC_CTYPE=C.UTF-8
+export WORK="${WORK:-${PWD}/build}"
 
-export CC=${CC:-clang}
-export CXX=${CXX:-clang++}
-clang_version="$($CC --version | sed -nr 's/.*version ([^ ]+?) .*/\1/p' | sed -r 's/-$//')"
-
-SANITIZER=${SANITIZER:-address -fsanitize-address-use-after-scope}
-flags="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=$SANITIZER"
-
-clang_lib="/usr/lib64/clang/${clang_version}/lib/linux"
-[ -d "$clang_lib" ] || clang_lib="/usr/lib/clang/${clang_version}/lib/linux"
-
-export CFLAGS=${CFLAGS:-$flags}
-export CXXFLAGS=${CXXFLAGS:-$flags}
-export LDFLAGS=${LDFLAGS:--L${clang_lib}}
-
-export WORK=${WORK:-$(pwd)}
-export OUT=${OUT:-$(pwd)/out}
-mkdir -p $OUT
-
-build=$WORK/build
-rm -rf $build
-mkdir -p $build
-
-if [ -z "$FUZZING_ENGINE" ]; then
-    fuzzflag="llvm-fuzz=enabled"
+if [[ -d "${WORK:?}" ]]; then
+    find "${WORK}" -mindepth 1 -print -delete
+else
+    mkdir "$WORK"
 fi
 
-if ! meson $build -D$fuzzflag ; then
-    cat $build/meson-logs/meson-log.txt
+config=(
+    --default-library=static
+    -Dprefix="${OUT:?}"
+
+    -Dfuzzing=enabled
+    -Dfuzzing-engine="${LIB_FUZZING_ENGINE:?}"
+    -Dfuzzing-install-dir="${OUT:?}"
+
+    # Fails to build on Ubuntu 16.04
+    -Dtools=disabled
+
+    # Don't use "-Wl,--no-undefined"
+    -Db_lundef=false
+    )
+
+if ! meson setup "${config[@]}" -- "$WORK"; then
+    cat "${WORK}/meson-logs/meson-log.txt" >&2 || :
     exit 1
 fi
 
-ninja -v -C $build
+meson compile -C "$WORK" -v
+
+meson install -C "$WORK"
+
+exit 0
