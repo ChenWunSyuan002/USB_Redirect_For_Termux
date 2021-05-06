456d2715973fa28fc4daf70378c125825645e193
Michael Hanselmann <public@hansmi.ch>
2021-05-06 19:50:45 +0200

    oss-fuzz: Build in subdirectory, remove built files
    Commit 8183a65 tried to use Meson's "--reconfigure" flag to avoid having to
    remove all files in "$WORK". Unfortunately doing so had a consequence not
    noticed at the time: Changed build options no longer cause a rebuild. When
    selecting another fuzzing engine linker errors would ensue.
    
    With this change the work of commit 8183a65 is partially undone. The build is
    now happening in a subdirectory of "$WORK", leaving OSS-Fuzz' own content alone,
    and it's once again fine to remove all object files, libraries and binary built
    by Meson.
    
    Signed-off-by: Michael Hanselmann <public@hansmi.ch>

--- build-aux/oss-fuzz.sh
+++ build-aux/oss-fuzz.sh
@@ -4,7 +4,34 @@
 set -eux -o pipefail
 
 export LC_CTYPE=C.UTF-8
-export WORK="${WORK:-${PWD}/build}"
+
+if [[ -z "${WORK:-}" ]]; then
+    builddir="${PWD}/build"
+else
+    builddir="${WORK}/build"
+fi
+
+if [[ -d "$builddir" ]]; then
+    # Meson only looks at the value of environment variables when options
+    # are defined for the first time, i.e. not on reconfiguration (see also
+    # https://bugs.freedesktop.org/show_bug.cgi?id=107313#c2). Consequently
+    # object files aren't rebuilt when such options change, e.g. "c_args" from
+    # "$CFLAGS". Removing all files in the build directory obviously solves that
+    # problem.
+    #
+    # usbredir is sufficiently small to make a full rebuild acceptable. If that
+    # were to change there'd be the following options:
+    #
+    # a) Use a build directory per build option set, e.g. by naming the
+    # directory after a hash of the various relevant environment variables.
+    #
+    # b) Explicitly pass environment variables as their target option, e.g.
+    # "-Dc_args=$CFLAGS". Relevant documentation:
+    # https://mesonbuild.com/Reference-tables.html#language-arguments-parameter-names
+    # https://mesonbuild.com/Reference-tables.html#compiler-and-linker-flag-environment-variables
+    #
+    find "$builddir" -mindepth 1 -print -delete
+fi
 
 config=(
     --default-library=static
@@ -21,22 +48,13 @@ config=(
     -Db_lundef=false
     )
 
-# Meson doesn't apply changed options without "--reconfigure" or "--wipe", but
-# those fail when the build directory is not already configured. This issue can
-# be avoided by checking for a configured build directory first.
-#
-# https://github.com/mesonbuild/meson/issues/7261
-if [[ -d "$WORK" ]] && meson introspect --projectinfo -- "$WORK"; then
-    config+=( --reconfigure )
-fi
-
-if ! meson setup "${config[@]}" -- "$WORK"; then
-    cat "${WORK}/meson-logs/meson-log.txt" >&2 || :
+if ! meson setup "${config[@]}" -- "$builddir"; then
+    cat "${builddir}/meson-logs/meson-log.txt" >&2 || :
     exit 1
 fi
 
-meson compile -C "$WORK" -v
+meson compile -C "$builddir" -v
 
-meson install -C "$WORK"
+meson install -C "$builddir"
 
 exit 0
