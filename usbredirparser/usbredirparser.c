ef3745f926f5b705b81f12e128f15af0106dae27
Michael Hanselmann <public@hansmi.ch>
2021-06-15 13:04:50 +0200

    usbredirparser: free parser's data on destroy
    Ref: https://gitlab.freedesktop.org/spice/usbredir/-/merge_requests/25#note_962929
    
    Signed-off-by: Michael Hanselmann <public@hansmi.ch>
    Signed-off-by: Victor Toso <victortoso@redhat.com>

--- usbredirparser/usbredirparser.c
+++ usbredirparser/usbredirparser.c
@@ -201,6 +201,9 @@ void usbredirparser_destroy(struct usbredirparser *parser_pub)
         (struct usbredirparser_priv *)parser_pub;
     struct usbredirparser_buf *wbuf, *next_wbuf;
 
+    free(parser->data);
+    parser->data = NULL;
+
     wbuf = parser->write_buf;
     while (wbuf) {
         next_wbuf = wbuf->next;
