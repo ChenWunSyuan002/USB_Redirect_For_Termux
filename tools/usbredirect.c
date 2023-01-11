ChenWunSyuan002/USB_Redirect_For_Termux

commit ebfcffbee055ef5ddf981b77240223790dcc0140

usbredirect: allow multiple devices by vendor:product
Currently, if an user tries to redirect two devices with the same
vendor:product info, the second instance of usbredirect will not
succeed, leading to a segmentation fault.

The core of the problem is that usbredirect is using
libusb_open_device_with_vid_pid() which always returns the first
instance of a given vendor:product, leading the second instance of
usbredirect to give an usb device that is in use to usbredirhost.

This patch fixes the situation, making it possible to run usbredirect
with --device $vendor:$product multiple times. We do a early check
that we can claim the usb device, prior to handle it over to
usbredirhost.

Related: https://gitlab.freedesktop.org/spice/usbredir/-/issues/29
Signed-off-by: Victor Toso <victortoso@redhat.com>

tools/usbredirect.c
@@ -467,6 +467,90 @@ signal_handler(gpointer user_data)
 }
 #endif
 
+static bool
+can_claim_usb_device(libusb_device *dev, libusb_device_handle **handle)
+{
+    int ret = libusb_open(dev, handle);
+    if (ret != 0) {
+        g_debug("Failed to open device");
+        return false;
+    }
+
+    /* Opening is not enough. We need to check if device can be claimed
+     * for I/O operations */
+    struct libusb_config_descriptor *config = NULL;
+    ret = libusb_get_active_config_descriptor(dev, &config);
+    if (ret != 0 || config == NULL) {
+        g_debug("Failed to get active descriptor");
+        *handle = NULL;
+        return false;
+    }
+
+#if LIBUSBX_API_VERSION >= 0x01000102
+    libusb_set_auto_detach_kernel_driver(*handle, 1);
+#endif
+
+    int i;
+    for (i = 0; i < config->bNumInterfaces; i++) {
+        int interface_num = config->interface[i].altsetting[0].bInterfaceNumber;
+#if LIBUSBX_API_VERSION < 0x01000102
+        ret = libusb_detach_kernel_driver(handle, interface_num);
+        if (ret != 0 && ret != LIBUSB_ERROR_NOT_FOUND
+            && ret != LIBUSB_ERROR_NOT_SUPPORTED) {
+            g_error("failed to detach driver from interface %d: %s",
+                    interface_num, libusb_error_name(ret));
+            *handle = NULL;
+            break
+        }
+#endif
+        ret = libusb_claim_interface(*handle, interface_num);
+        if (ret != 0) {
+            g_debug("Could not claim interface");
+            *handle = NULL;
+            break;
+        }
+        ret = libusb_release_interface(*handle, interface_num);
+        if (ret != 0) {
+            g_debug("Could not release interface");
+            *handle = NULL;
+            break;
+        }
+    }
+
+    libusb_free_config_descriptor(config);
+    return *handle != NULL;
+}
+
+static libusb_device_handle *
+open_usb_device(redirect *self)
+{
+    struct libusb_device **devs;
+    struct libusb_device_handle *dev_handle = NULL;
+    size_t i, ndevices;
+
+    ndevices = libusb_get_device_list(NULL, &devs);
+    for (i = 0; i < ndevices; i++) {
+        struct libusb_device_descriptor desc;
+        if (libusb_get_device_descriptor(devs[i], &desc) != 0) {
+            g_warning("Failed to get descriptor");
+            continue;
+        }
+
+        if (self->device.vendor != desc.idVendor ||
+            self->device.product != desc.idProduct) {
+            continue;
+        }
+
+        if (can_claim_usb_device(devs[i], &dev_handle)) {
+            break;
+        }
+    }
+
+    libusb_free_device_list(devs, 1);
+    return dev_handle;
+}
+
+
 static gboolean
 connection_incoming_cb(GSocketService    *service,
                        GSocketConnection *client_connection,
@@ -516,11 +600,7 @@ main(int argc, char *argv[])
     g_unix_signal_add(SIGTERM, signal_handler, self);
 #endif
 
-    /* This is binary is not meant to support plugins so it is safe to pass
-     * NULL as libusb_context here and all subsequent calls */
-    libusb_device_handle *device_handle = libusb_open_device_with_vid_pid(NULL,
-            self->device.vendor,
-            self->device.product);
+    libusb_device_handle *device_handle = open_usb_device(self);
     if (!device_handle) {
         g_printerr("Failed to open device!\n");
         goto err_init;

commit 39fca2b6582c231c1579b1559b322a5c8a15eebb

usbredirect: Fix leak of libusb_device_handle
==30494== 24 bytes in 1 blocks are definitely lost in loss record 116 of 288
==30494==    at 0x484386F: malloc (vg_replace_malloc.c:393)
==30494==    by 0x4883B6E: usbi_add_event_source (io.c:2654)
==30494==    by 0x48885E7: op_open (linux_usbfs.c:1410)
==30494==    by 0x4888193: libusb_open (core.c:1318)
==30494==    by 0x40268E: can_claim_usb_device (usbredirect.c:462)
==30494==    by 0x40268E: open_usb_device (usbredirect.c:540)
==30494==    by 0x40268E: main (usbredirect.c:599)
==30494==
==30494== 96 bytes in 1 blocks are definitely lost in loss record 262 of 288
==30494==    at 0x4848464: calloc (vg_replace_malloc.c:1340)
==30494==    by 0x4888161: libusb_open (core.c:1310)
==30494==    by 0x40268E: can_claim_usb_device (usbredirect.c:462)
==30494==    by 0x40268E: open_usb_device (usbredirect.c:540)
==30494==    by 0x40268E: main (usbredirect.c:599)
==30494==
==30494== 809 (128 direct, 681 indirect) bytes in 1 blocks are definitely lost in loss record 284 of 288
==30494==    at 0x4848464: calloc (vg_replace_malloc.c:1340)
==30494==    by 0x4887C78: usbi_alloc_device (core.c:708)
==30494==    by 0x4888961: linux_enumerate_device.isra.0 (linux_usbfs.c:1113)
==30494==    by 0x4889114: UnknownInlinedFun (linux_udev.c:299)
==30494==    by 0x4889114: UnknownInlinedFun (linux_usbfs.c:458)
==30494==    by 0x4889114: op_init (linux_usbfs.c:410)
==30494==    by 0x4889E78: libusb_init (core.c:2353)
==30494==    by 0x402593: main (usbredirect.c:571)

Fixes: ebfcffbee055ef5ddf981b77240223790dcc0140
Reported-by: Uri Lublin <uril@redhat.com>
Signed-off-by: Victor Toso <victortoso@redhat.com>

tools/usbredirect.c
@@ -471,8 +471,7 @@ can_claim_usb_device(libusb_device *dev, libusb_device_handle **handle)
     ret = libusb_get_active_config_descriptor(dev, &config);
     if (ret != 0 || config == NULL) {
         g_debug("Failed to get active descriptor");
-        *handle = NULL;
-        return false;
+        goto fail;
     }
 
 #if LIBUSBX_API_VERSION >= 0x01000102
@@ -488,26 +487,29 @@ can_claim_usb_device(libusb_device *dev, libusb_device_handle **handle)
             && ret != LIBUSB_ERROR_NOT_SUPPORTED) {
             g_error("failed to detach driver from interface %d: %s",
                     interface_num, libusb_error_name(ret));
-            *handle = NULL;
-            break
+            goto fail;
         }
 #endif
         ret = libusb_claim_interface(*handle, interface_num);
         if (ret != 0) {
             g_debug("Could not claim interface");
-            *handle = NULL;
-            break;
+            goto fail;
         }
         ret = libusb_release_interface(*handle, interface_num);
         if (ret != 0) {
             g_debug("Could not release interface");
-            *handle = NULL;
-            break;
+            goto fail;
         }
     }
 
     libusb_free_config_descriptor(config);
-    return *handle != NULL;
+    return true;
+
+fail:
+    libusb_free_config_descriptor(config);
+    libusb_close(*handle);
+    *handle = NULL;
+    return false;
 }
 
 static libusb_device_handle *